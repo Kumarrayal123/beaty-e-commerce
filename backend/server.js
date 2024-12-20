db9015da02164f9b1094308bb3c00e9b94f8e4cb
HeyBroooo <Mohitkumar4049a@gmail.com>
2024-12-20 14:19:58 +0530

    Add email sending functionality and nodemailer integration

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import sendMailRouter from './routes/sendMail.js';
 
 // App config
 const app = express();
@@ -72,6 +73,7 @@ app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
+app.use('/api/send-mail',sendMailRouter)
 
 
 // API endpoint for health check

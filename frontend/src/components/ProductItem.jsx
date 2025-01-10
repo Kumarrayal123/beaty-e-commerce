77cf17ef8de9ebc00567da23e167de25af519d4f
Kumarrayal123 <kardamkumar13@gmail.com>
2025-01-10 19:15:59 +0530

    kk

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -28,11 +28,15 @@ const ProductItem = ({ id, image, name, price }) => {
   return (
     <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
       <div className='overflow-hidden'>
-        <img className='hover:scale-110 transition ease-in-out rounded-lg shadow-lg' src={image} alt={name} />
+        <img className="group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105" src={image} alt={name} />
       </div>
+      
+      
       <p className='pt-3 pb-1 text-sm'>{name}</p>
       <p className='text-sm font-medium'>{currency}{price}</p>
+      
     </Link>
+    
   );
 };
 
--- frontend/src/pages/PureSilkSaree.jsx
+++ frontend/src/pages/PureSilkSaree.jsx
@@ -94,12 +94,7 @@ const PureSilkSaree = () => {
               />
               {/* Add to Cart Button */}
 
-              {/* <button
-                className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-black px-2 py-2 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-                onClick={() =>handleAddToCart(product)}
-              >
-                Add to Cart
-              </button> */}
+             
               <div className="bg bg-opacity-50 text-center p-2 opacity-1 duration-500 ease-in-out">
                 <button
 
